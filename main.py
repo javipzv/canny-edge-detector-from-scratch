64e160e2cc5e1a806cf1952025930337f37cd32c
javipzv <90279135+javipzv@users.noreply.github.com>
2023-11-03 21:17:10 +0100

    added examples

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 import math
 from scipy import signal
 
-img = cv2.imread('example_inputs/persona.jpg')
+img = cv2.imread('example_inputs/kid.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-borders = CannyEdgeDetector(img=img_rgb, gaussian_size=9, sigma=2, min_threshold=40, max_threshold=60)
+borders = CannyEdgeDetector(img=img_rgb, gaussian_size=11, sigma=3, min_threshold=10, max_threshold=25)
 
 plt.imshow(borders, cmap='gray')
 plt.show()
+
+cv2.imwrite('example_outputs/kid_borders.jpg', borders)
